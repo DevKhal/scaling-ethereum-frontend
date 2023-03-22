135203636141284e42b260d7a9d0c3c249ef4363
Ryan O'Toole <ryanotoole@Ryans-Air.attlocal.net>
2023-03-22 11:26:04 -0500

    customized frontend

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,12 +9,11 @@ export default function Home() {
   return (
     <>
       <div className="px-4 py-5 my-5 text-center">
-        <h1 className="display-5 fw-bold">Centered hero</h1>
+        <h1 className="display-5 fw-bold">PrivateSwap</h1>
         <div className="col-lg-6 mx-auto">
-          <p className="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+          <p className="lead mb-4">Quickly swap with privacy and confidence. Swap on the same chain or cross chains the choice is yours. Using cutting edge ERC-4337 to optimize swaps for gas efficiency and provide the privacy that everyone not only needs but deserves. </p>
           <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-            <button type="button" className="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
-            <button type="button" className="btn btn-outline-secondary btn-lg px-4">Secondary</button>
+            <button type="button" className="btn btn-primary btn-lg px-4 gap-3">Click Here to Begin</button>
           </div>
         </div>
       </div>
